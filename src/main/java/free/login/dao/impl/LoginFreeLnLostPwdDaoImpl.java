d71f465420e240c7918377366ab9e738868c5846
Jimmy Kang <kchulpan@naver.com>
2018-10-19 18:34:05 +0900

    로딩에러 수정

--- src/main/java/free/login/dao/impl/LoginFreeLnLostPwdDaoImpl.java
+++ src/main/java/free/login/dao/impl/LoginFreeLnLostPwdDaoImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import free.login.dao.LoginFreeLnLostPwdDao;
 
-@Repository
+@Repository("loginFreeLnLostPwdDao")
 public class LoginFreeLnLostPwdDaoImpl implements LoginFreeLnLostPwdDao
 {
 	@Autowired
